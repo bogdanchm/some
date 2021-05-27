d7ea8a9b1afdae7ca74ef00b2ec10d0a22beab05
BogdanChesche <bogdan.chesche@tremend.com>
2021-05-27 16:33:40 +0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Yoo this is how we doll
         </p>
         <a
           className="App-link"
